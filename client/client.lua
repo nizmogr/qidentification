2044db470a2ec79bc46f9ac8a232523c3ca8c5b9
PERPGamer <copsclanleaderHD@gmail.com>
2022-02-01 18:45:46 -0500

    Fixed Client.lua

--- client/client.lua
+++ client/client.lua
@@ -22,37 +22,6 @@ exports('identification', function(data, slot)
 	else
 		ox_inventory:notify({text = 'License is in cooldown.'})
 	end
-
-exports('Showidentification',function (data,slot)
-    exports.ox_inventory:useItem(data, function(data)
-        if data then
-            local item = exports.ox_inventory:Items("identification")
-			if item then
-				TriggerEvent('qidentification:showID',item)
-			end
-        end
-    end)
-end)
-exports('Showdrivers_license',function (data,slot)
-    exports.ox_inventory:useItem(data, function(data)
-        if data then
-            local item = exports.ox_inventory:Items("drivers_license")
-			if item then
-				TriggerEvent('qidentification:showID',item)
-			end
-        end
-    end)
-end)
-exports('Showfirearms_license',function (data,slot)
-    exports.ox_inventory:useItem(data, function(data)
-        if data then
-            local item = exports.ox_inventory:Items("firearms_license")
-			if item then
-				TriggerEvent('qidentification:showID',item)
-			end
-        end
-    end)
-
 end)
 
 -- Event to show your ID to nearby players
